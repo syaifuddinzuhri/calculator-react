543453a0c20754f9bb5be5745907fe3d05465871
syaifuddinzuhri <mochammadsyaifuddinzuhri@gmail.com>
2020-09-28 23:35:46 +0700

    first commit

---
+++ src/components/Button/ClearButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ClearButton.css";
+
+export const ClearButton = (props) => {
+  return (
+    <div className="clear-btn" onClick={props.handleClear}>
+      {props.children}
+    </div>
+  );
+};
---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./input.css";
+
+export const Input = (props) => {
+  return <div className="input">{props.input}</div>;
+};
